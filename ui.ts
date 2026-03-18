5503bf8fd588089182dd2c677b983872bbc76aa5
apburnie <apburnie@hotmail.co.uk>
2026-03-18 17:41:50 +0000

    memory using JSON -- issue is that this breaks with minor variations in AI response

--- ui.ts
+++ ui.ts
@@ -24,6 +24,7 @@ interface Data {
   os: string;
   hardware: string;
   modelStatus: "UNLOADED" | "LOADED" | "PROCESSING";
+  brainKG: { subject: string; predicate: string; object: string }[];
 }
 
 const main = `${Home()}${Converse()}`;
@@ -36,24 +37,31 @@ const data: () => Data = () => ({
   os: "UBUNTU",
   hardware: "vulkan",
   modelStatus: "UNLOADED",
+  brainKG: [{ subject: "Assistant", predicate: "is called", object: "Gaius" }],
   main,
   func_s: {
-    async submitPrompt(data: Data) {
-      data.modelStatus = "PROCESSING";
-      const { hist, prompt } = data;
+    async buildMemory(data: Data) {
+      const first_hist = {
+        role: "system",
+        content: `Your aim is to improve a subject-predicate-object knowledge graph with provided information.
+        Your start with the following knowledge graph: ${JSON.stringify({ data: data.brainKG })}
+        Only output the knowledge graph in the following JSON format: {"data": {"subject": string, "predicate": string, "object": string}[] }.
 
-      const u_step = hist.length;
-      const a_step = hist.length + 1;
-      hist.push({
-        step: u_step,
-        role: "user",
-        content: prompt,
-        convert_content: Dompurify.sanitize(await parse(prompt)),
-      });
+        `,
+      };
+
+      const last_prompt = data.hist[data.hist.length - 1];
+
+      const input_hist = [
+        first_hist,
+        { role: "user", content: last_prompt?.content },
+      ];
+
+      console.log(input_hist);
 
       const body = {
         model: MODEL,
-        messages: hist,
+        messages: input_hist,
       };
 
       const resp = await fetch(
@@ -71,7 +79,67 @@ const data: () => Data = () => ({
       const json: { choices: { message: { content: string } }[] } =
         await resp.json();
 
-      console.log("RESPONSE", json);
+      console.log("KG ans", json);
+
+      const assistant_answer = json.choices[0]?.message.content || "";
+
+      console.log("store assistant answer in brain:", assistant_answer);
+
+      const proc_assistant_answer = assistant_answer.substring(
+        assistant_answer.indexOf("{"),
+        assistant_answer.lastIndexOf("}") + 1,
+      );
+
+      console.log(proc_assistant_answer);
+
+      const new_data = JSON.parse(proc_assistant_answer);
+
+      data.brainKG = [...data.brainKG, ...new_data.data];
+    },
+    async submitPrompt(data: Data) {
+      const first_hist = {
+        role: "system",
+        content: `
+        You are a helpful assistant. You have been provided with the following knowledge graph ${JSON.stringify({ data: data.brainKG })}.
+        Use this knowledge graph and your own knowledge to answer questions. `,
+      };
+
+      data.modelStatus = "PROCESSING";
+      const { hist, prompt } = data;
+
+      const u_step = hist.length;
+      const a_step = hist.length + 1;
+
+      const user_prompt = {
+        role: "user",
+        content: prompt,
+      };
+
+      hist.push({
+        step: u_step,
+        convert_content: Dompurify.sanitize(await parse(prompt)),
+        ...user_prompt,
+      });
+
+      const body = {
+        model: MODEL,
+        messages: [first_hist, user_prompt],
+      };
+
+      const resp = await fetch(
+        `http://localhost:${SERVER_PORT}/v1/chat/completions`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer no-key",
+          },
+          body: JSON.stringify(body),
+        },
+      );
+
+      const json: { choices: { message: { content: string } }[] } =
+        await resp.json();
 
       const assistant_answer = json.choices[0]?.message.content || "";
 
@@ -81,6 +149,9 @@ const data: () => Data = () => ({
         content: assistant_answer,
         convert_content: Dompurify.sanitize(await parse(assistant_answer)),
       });
+
+      await this.buildMemory!(data);
+
       data.prompt = "";
 
       data.modelStatus = "LOADED";
