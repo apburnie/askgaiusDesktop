d56830f2a546018c8a5bb84c3a9fc168ffadbb6a
apburnie <apburnie@hotmail.co.uk>
2026-04-08 17:23:41 +0100

    adds branding to app

--- component/page/Home.ts
+++ component/page/Home.ts
@@ -1,6 +1,8 @@
 import { GaiusButton } from "../atom";
+import Logo from "../asset/AskGaiusLogo.svg";
 export default function Home() {
   return `<home-container x-show="page === 'HOME'">
+  <img style="height: 50%" src=${Logo} alt="AskGaius Logo"/>
     ${GaiusButton({ colour: "blue", text: "New", func: "() => func_s.startNewSession($data) " })}
     ${GaiusButton({ colour: "blue", text: "Load", func: "() => {page = 'CONTINUE'; }" })}
     ${GaiusButton({ colour: "blue", text: "Download Backup", func: "async () => await func_s.downloadBackup()" })}
