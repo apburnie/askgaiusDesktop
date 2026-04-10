11cefdf3214f78ac1f607d7125b0a002531bf2b9
apburnie <apburnie@hotmail.co.uk>
2026-04-10 17:31:45 +0100

    enlarge buttons

--- component/molecule/ActionCol.ts
+++ component/molecule/ActionCol.ts
@@ -2,7 +2,7 @@ import { GaiusButton } from "../atom";
 
 export default function ActionCol() {
   return `<action-col>
-  ${GaiusButton({ colour: "green", text: "&uarr;", func: "func_s.submitPrompt($data)" })}
-  ${GaiusButton({ colour: "blue", text: "🗈", func: "() => {$data.converseSubPage = 'SETTING'}" })}
+  ${GaiusButton({ colour: "green", text: "&uarr;", func: "func_s.submitPrompt($data)", attr: { style: "font-size:2rem;" } })}
+  ${GaiusButton({ colour: "blue", text: "🗈", func: "() => {$data.converseSubPage = 'SETTING'}", attr: { style: "font-size:2rem;" } })}
   </action-col>`;
 }
